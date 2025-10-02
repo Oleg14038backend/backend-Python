092cb794c4c1f6755faaa6baede678a9c9f2b62b
Oleg <119931573+oleg14038@users.noreply.github.com>
2025-10-02 12:58:00 +0900

    upd

--- script.py
+++ script.py
@@ -1,15 +1,23 @@
-from github import Github
+from github import Github, Auth
 import os
 
-# токен GitHub (создаётся в Settings -> Developer settings -> PAT)
-g = Github("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")
+# Берём токен из переменной окружения
+token = os.getenv("")
+if not token:
+    raise ValueError("⚠️ Не найден токен. Установи переменную окружения")
+
+auth = Auth.Token(token)
+g = Github(auth=auth)
+
 repo = g.get_user().get_repo("backend-Python")
 
 with open("issues.md", "r", encoding="utf-8") as f:
     content = f.read().split("### #")
 
-for block in content[1:]:  # первый кусок до ### #3 — шапка
+for block in content[1:]:
     lines = block.splitlines()
     title = "#" + lines[0].strip()
     body = "\n".join(lines[1:])
     repo.create_issue(title=title, body=body)
+
+print("✅ Все Issues успешно созданы!")
