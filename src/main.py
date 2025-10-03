446052cf9a3ea1444f9c182492d59e8098051c3f
Oleg <119931573+oleg14038@users.noreply.github.com>
2025-10-03 19:17:19 +0900

    feat: add main.py module

---
+++ src/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Основной модуль приложения."""
+
+
+def main() -> None:
+    """Точка входа в приложение."""
+    print("Hello, World!")
+    return None
+
+
+if __name__ == "__main__":
+    main()
